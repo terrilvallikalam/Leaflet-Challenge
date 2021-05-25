08b1ff9cf3b37a78a6bb63bdc82b04900d03e5d7
Terril Vallikalam <terrilvallikalam@Terrils-MacBook-Pro.local>
2021-05-24 20:46:47 -0500

    first attempt

---
+++ Leaflet-Level1/logic.js
@@ -0,0 +1,11 @@
+// Create a map object.
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+});
+
+// Add a tile layer.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
